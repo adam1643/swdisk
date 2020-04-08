faae2c91340129c6e70b0aef4ace733de3c6379b
Adam Stanislawski <adam@asweb.pl>
2020-04-08 13:19:00 +0200

    Switch to new GUI

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-from nonogram import Nonogram
-from gui import GUI
+from gui_main import GUIMain
 
-# init game and load test puzzle
-game = Nonogram()
-game.load_from_file('test.txt')
 
-# init GUI
-g = GUI(game=game)
+g = GUIMain()
 g.set_layout()
-g.reload()
-g.redraw_hints(game.rows, game.cols)
-g.redraw()
-
-# loop for event handling of GUI
 while g.event_handler():
     pass
 
